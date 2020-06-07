9b75f4a8ce6cc585dc62d65e41e2e708c4f9570d
alekhya <alekhya.rudra@cybrilla.com>
2020-06-07 19:45:10 +0530

    adds bootstrap, popper js and jquery and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authorized
+  # before_action :authorized
   helper_method :current_user
   helper_method :logged_in?
 
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 
-  def authorized
-   redirect_to '/welcome' unless logged_in?
-  end
+  # def authorized
+   # redirect_to root_path unless logged_in?
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :users, only: [:new, :create]
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
-  get 'authorized', to: 'sessions#page_requires_login'
 
-  root to: "sessions#welcome"
+  root to: 'home#index'
 
 end
